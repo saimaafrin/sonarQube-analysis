c3ed5f88b00284dce2f89162b9fdd23480cd8cf1
Saima Afrin <safrin@wm.edu>
2025-02-18 22:08:31 +0000

    config

--- py-codellama7B-AWQ-all/taskid-27_flip_case-gen8.py
+++
@@ -1,7 +0,0 @@
-def flip_case(string: str) -> str:
-    """ For a given string, flip lowercase characters to uppercase and uppercase to lowercase.
-    >>> flip_case('Hello')
-    'hELLO'
-    """
-
-    return "".join([x.lower() if x.isupper() else x.upper() for x in string])
