aff99c93541756afecc619ad0d1a7ffb7b689150
Saima Afrin <safrin@wm.edu>
2025-02-20 04:29:26 +0000

    fix

--- py-codellama13B-FP-all/taskid-11_string_xor-gen5.py
+++
@@ -1,9 +0,0 @@
-def string_xor(a: str, b: str) -> str:
-    """ Input are two strings a and b consisting only of 1s and 0s.
-    Perform binary XOR on these inputs and return result also as a string.
-    >>> string_xor('010', '110')
-    '100'
-    """
-
-    assert len(a) == len(b), 'string length should be the same'
-    return ''.join(str(int(i) ^ int(j)) for i, j in zip(a, b))
--- py-codellama13B-FP-all/taskid-11_string_xor-gen8.py
+++
@@ -1,9 +0,0 @@
-def string_xor(a: str, b: str) -> str:
-    """ Input are two strings a and b consisting only of 1s and 0s.
-    Perform binary XOR on these inputs and return result also as a string.
-    >>> string_xor('010', '110')
-    '100'
-    """
-
-    assert len(a) == len(b)
-    return ''.join(str(int(x) ^ int(y)) for x, y in zip(a, b))
