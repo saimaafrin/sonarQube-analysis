e563a194f194a6e335513507daad5803bdb6d2c5
Saima Afrin <safrin@wm.edu>
2025-02-20 04:28:43 +0000

    fix

--- py-codellama7B-AWQ-all/taskid-11_string_xor-gen12.py
+++
@@ -1,8 +0,0 @@
-def string_xor(a: str, b: str) -> str:
-    """ Input are two strings a and b consisting only of 1s and 0s.
-    Perform binary XOR on these inputs and return result also as a string.
-    >>> string_xor('010', '110')
-    '100'
-    """
-
-    raise NotImplementedError
