3dd438ece4d0d0a393c7f1f50e6622472a2d9c6b
Saima Afrin <safrin@wm.edu>
2025-02-20 04:29:14 +0000

    fix

--- py-codellama13B-AWQ-all/taskid-24_largest_divisor-gen2.py
+++
@@ -1,7 +0,0 @@
-def largest_divisor(n: int) -> int:
-    """ For a given number n, find the largest number that divides n evenly, smaller than n
-    >>> largest_divisor(15)
-    5
-    """
-
-    return next(divisor for divisor in range(n, 1, -1) if n % divisor == 0)
