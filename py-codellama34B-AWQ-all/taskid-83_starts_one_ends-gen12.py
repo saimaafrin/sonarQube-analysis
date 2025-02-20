5aac4232e1aaaa1ee82a27f06be918dc5b97cdbc
Saima Afrin <safrin@wm.edu>
2025-02-20 04:29:59 +0000

    fix

--- py-codellama34B-AWQ-all/taskid-83_starts_one_ends-gen12.py
+++
@@ -1,9 +0,0 @@
-def starts_one_ends(n: int) -> int:
-    """
-    Given a positive integer n, return the count of the numbers of n-digit
-    positive integers that start or end with 1.
-    """
-
-    assert n > 0
-
-    return (10 ** (n - 1) + 10 ** n) // 2
--- py-codellama34B-AWQ-all/taskid-83_starts_one_ends-gen18.py
+++
@@ -1,9 +0,0 @@
-def starts_one_ends(n: int) -> int:
-    """
-    Given a positive integer n, return the count of the numbers of n-digit
-    positive integers that start or end with 1.
-    """
-
-    # Get number of digits.
-    digits = len(str(n))
-    return pow(10, digits-1) + pow(10, digits-2)
