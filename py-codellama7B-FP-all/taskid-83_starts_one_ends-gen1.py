6c050bf2bdfce9aea3a5cdc4ff7f4de9bb8698b2
Saima Afrin <safrin@wm.edu>
2025-03-08 13:11:47 +0000

    rm py-codellama7B-FP-all

--- py-codellama7B-FP-all/taskid-83_starts_one_ends-gen1.py
+++
@@ -1,9 +0,0 @@
-def starts_one_ends(n: int) -> int:
-    """
-    Given a positive integer n, return the count of the numbers of n-digit
-    positive integers that start or end with 1.
-    """
-
-    if n < 1:
-        return 0
-    return _starts_one_ends(n)
