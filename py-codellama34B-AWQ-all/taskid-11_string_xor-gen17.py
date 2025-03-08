f3c18e79cc50b12688f338d6126373b49da4a32e
Saima Afrin <safrin@wm.edu>
2025-03-08 13:10:04 +0000

    py-codellama34B-AWQ-all

--- py-codellama34B-AWQ-all/taskid-11_string_xor-gen17.py
+++
@@ -1,9 +0,0 @@
-def string_xor(a: str, b: str) -> str:
-    """ Input are two strings a and b consisting only of 1s and 0s.
-    Perform binary XOR on these inputs and return result also as a string.
-    >>> string_xor('010', '110')
-    '100'
-    """
-
-    xor_result = int(a, 2) ^ int(b, 2)
-    return bin(xor_result)[2:].rjust(len(a), '0')
--- py-codellama34B-AWQ-all/taskid-83_starts_one_ends-gen15.py
+++
@@ -1,7 +0,0 @@
-def starts_one_ends(n: int) -> int:
-    """
-    Given a positive integer n, return the count of the numbers of n-digit
-    positive integers that start or end with 1.
-    """
-
-    return 2*n - 2
