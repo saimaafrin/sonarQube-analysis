e6abf313d28a77db58bd4a2acc4505e59a8e8103
Saima Afrin <safrin@wm.edu>
2025-03-08 13:10:35 +0000

    py-codellama34B-FP-all

--- py-codellama34B-FP-all/taskid-11_string_xor-gen4.py
+++
@@ -1,9 +0,0 @@
-def string_xor(a: str, b: str) -> str:
-    """ Input are two strings a and b consisting only of 1s and 0s.
-    Perform binary XOR on these inputs and return result also as a string.
-    >>> string_xor('010', '110')
-    '100'
-    """
-
-    # return ''.join('1' if (a[i] != b[i]) else '0' for i in range(len(a)))
-    return ''.join('1' if int(a[i]) ^ int(b[i]) else '0' for i in range(len(a)))
